89c80f31ec475903758a9d77b8fbb9fd5a97f1c8
AntonyJensonA <143073662+AntonyJensonA@users.noreply.github.com>
2026-09-20 15:21:21 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("Loan_model.pkl")
+
+st.title("Loan Approval Prediction")
+
+income = st.number_input(
+    "Enter the Income",
+    min_value=0.0,
+    value=50000.0,
+    step=1000.0
+)
+credit_score = st.number_input(
+    "Enter the Credit Score",
+    min_value=0.0,
+    value=720.0,
+    step=1.0
+)
+if st.button("Predict"):
+
+    input_data = pd.DataFrame({
+        "Income": [income],
+        "Credit_Score": [credit_score]
+    })
+    prediction = model.predict(input_data)
+    pred = prediction[0]
+    if pred == 1:
+        st.success("Loan Approved")
+    else:
+        st.error("Loan Not Approved")
